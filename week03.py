2f5f2383fc37383ed1c434efd8fa230bd6082898
deepblue <deepblue@inha.ac.kr>
2025-03-24 11:55:09 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,4 +1,18 @@
-city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-# city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-city = set(city)
-print(city)
+def duplicate_city(cities):
+    result = list()
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result.append(city)
+    return result
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+# cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities)))
