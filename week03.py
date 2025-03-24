bf58318a3050253088f914397f979794623545b7
deepblue <deepblue@inha.ac.kr>
2025-03-24 12:07:32 +0900

    v1.0 array

--- week03.py
+++ week03.py
@@ -1,18 +1,10 @@
-def duplicate_city(cities):
-    result = list()
-    s = set()
+def intersection(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result.append(city)
-    return result
-
-cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-# cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-cities.append('Anyang')
-cities.append('Seoul')
-print(cities)
-print(set(duplicate_city(cities)))
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
+print(intersection(l1, l2))
