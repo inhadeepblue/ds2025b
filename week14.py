39a147273fa29b8f1ab608e9a21334ebf136f59c
deepblue <deepblue@inha.ac.kr>
2025-06-09 10:35:13 +0900

    v5.4 boj 1929

--- week14.py
+++ week14.py
@@ -1,13 +1,21 @@
+import math
+import sys
+
 def is_prime(n) -> bool:
   if n <= 1:
       return False
+  elif n == 2:
+      return True
+  elif n % 2 == 0:
+      return False
   else:
-    for i in range(2, n):
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         if n % i == 0:
           return False
   return True
 
-s, e = map(int, input().split())
+#s, e = map(int, input().split())  
+s, e = map(int, sys.stdin.readline().split())  
 for i in range(s, e+1):
   if is_prime(i):
     print(i)
